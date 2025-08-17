ae02fe368d89c59be78654ebae05ee2b06df7f5a
Dovlet <ddowlet502@gmail.com>
2025-08-17 13:55:14 +0300

    Add files via upload

---
+++ http_client.py
@@ -0,0 +1,29 @@
+from aiohttp import ClientSession
+
+
+
+class HttpClient:
+    def __init__(self,base_url: str, api_key: str):
+        self.session = ClientSession(
+            base_url = base_url,
+            headers = {
+                'X-CMC_PRO_API_KEY': api_key,
+            }
+        )
+
+
+class CMCHttpClient(HttpClient):
+    async def get_listings(self):
+        async with self.session.get('/v1/cryptocurrency/listings/latest') as response:
+            result = await response.json()
+            return result['data']
+
+
+    async def get_currency(self, currency_id: int):
+        async with self.session.get(
+            url='/v2/cryptocurrency/quotes/latest',
+            params={'id': currency_id}
+        ) as response:
+            result = await response.json()
+            return result['data'],[str(currency_id)]
+    
